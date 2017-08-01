4ef4eece85a39b9bc5780f80fdc3ca6f801cbd7b
xiaomingyan <779328250@qq.com>
2017-08-01 16:31:07 +0800

    添加tools两个配置文件

---
+++ src/main/java/tools/ParseYamUntil.java
@@ -0,0 +1,34 @@
+package tools;
+
+import org.ho.yaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+
+/**
+ * Created by xmy on 2017/8/1.
+ */
+public class ParseYamUntil {
+        public  static HashMap<String, HashMap<String,String>> results;
+        public  static HashMap<String, HashMap<String,String>> settings;
+        public  static HashMap<String, HashMap<String,String>> locators;
+        public  static HashMap<String, HashMap<String,String>> parseLocatorFile(String FileName){
+            File file=new File("src/main/resources/locator/"+FileName+".yaml");
+            try {
+                locators= Yaml.loadType(new FileInputStream(file.getAbsolutePath()),HashMap.class);
+            }catch (Exception e){
+            e.printStackTrace();
+            }
+            return locators;
+        }
+        public HashMap<String, HashMap<String,String>> parseSettingFile(String FileName) {
+            File file = new File("src/main/resources/config/Setting.yaml");
+            try {
+                settings = Yaml.loadType(new FileInputStream(file.getAbsolutePath()), HashMap.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+                return settings;
+        }
+}
---
+++ src/main/java/tools/WebDriverUntil.java
@@ -0,0 +1,114 @@
+package tools;
+
+/**
+ * Created by xmy on 2017/8/1.
+ */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import  static  tools.ParseYamUntil.locators;
+import  static  tools.ParseYamUntil.settings;
+public class WebDriverUntil {
+
+        private  static  String browser_type;
+        private  static  String  site_url;
+    /**
+     *  初始化获取配置文件
+     *  @param  settingFileName;
+     */
+        public  static  void init(String settingFileName){
+            settings=ParseYamUntil.parseLocatorFile(settingFileName);
+            browser_type=settings.get("browser_type").get("value");
+            site_url=settings.get("site_url").get("value");
+        }
+    /**
+     *  获取driver
+     * @return
+     */  public  static WebDriver getWebDriver(){
+        WebDriver driver=null;
+        if ("chrome".equals(browser_type)){
+            driver= new ChromeDriver();
+        }else if("IE".equals(browser_type)){
+            driver=new InternetExplorerDriver();
+        }else  if("fireFox".equals(browser_type)){
+            driver= new FirefoxDriver();
+        }else{
+            System.out.println("not found browsertype please check browsertype");
+        }
+         return  driver;
+    }
+    /**
+     *  停止driver
+     * @param  driver
+     */
+        public  static void stop(WebDriver driver){
+            if (driver==null){
+                System.out.println("the driver is not exist");
+            }
+            System.out.println("driver start");
+            driver.quit();
+        }
+    /**
+     *  开始driver
+     * @param  driver
+     */
+        public  static  void start(WebDriver driver){
+            if (driver==null){
+                System.out.println("the driver is null");
+            }
+            System.out.println("driver start");
+            driver.get(site_url);
+        }
+    /**
+     *  获取element
+     * @param  key
+     * @param  locatorFileName
+     * @return
+     */
+    public static WebElement getElement(WebDriver driver,String key,String locatorFileName){
+        if (driver==null){
+            System.out.println("driver is not exist");
+            return  null;
+        }
+        locators= ParseYamUntil.parseLocatorFile((locatorFileName));
+        String type=locators.get(key).get("type");
+        String value=locators.get(key).get("value");
+        WebDriverWait wait= new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(getBy(type,value)));
+        WebElement webElement=driver.findElement(getBy(type,value));
+        return  webElement;
+    }
+    /**
+     *定位方法
+     * @param type
+     * @param value
+     * @return
+     */
+    public static By getBy(String type,String value) {
+        By by = null;
+        if ("xpath".equalsIgnoreCase(type)) {
+            by = new By.ByXPath(value);
+        } else if ("id".equalsIgnoreCase(type)) {
+            by = new By.ById(value);
+        } else if ("className".equalsIgnoreCase(type)) {
+            by = new By.ByClassName(value);
+        } else if ("cssSelector".equalsIgnoreCase(type)) {
+            by = new By.ByCssSelector(value);
+        } else if ("linkText".equalsIgnoreCase(type)) {
+            by = new By.ByLinkText(value);
+        } else if ("name".equalsIgnoreCase(type)) {
+            by = new By.ByName(value);
+        } else if ("tagName".equalsIgnoreCase(type)) {
+            by = new By.ByTagName(value);
+        } else if ("partialLinkTex".equalsIgnoreCase(type)) {
+            by = new By.ByPartialLinkText(value);
+        }
+            return by;
+        }
+    }
